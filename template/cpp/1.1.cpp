f1351a35467e2ec31189a415f39815ac4340179e
atyoung1997 <atyoung1997@gmail.com>
2023-02-24 22:19:27 -0600

    template folder

---
+++ template/cpp/1.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+#include <map>
+#include <typeinfo>
+#include <algorithm>
+
+// solution 1
+
+int main(){
+    return 1;
+}
---
+++ template/py/1.1.py
@@ -0,0 +1,36 @@
+from leetcode_helpers import run_test_scenarios
+# Q: 
+# Clarification Questions:
+
+# Test Cases:
+tests = {
+    True : 
+    [
+        "abcdefgh",
+        "oen luf"
+    ],
+    
+    False : 
+    [
+        "repeat",
+        "random letters"
+    ]
+}
+
+# A1: 
+# Description: 
+# Time:  
+# Space: 
+
+run_test_scenarios(, tests, "")
+
+# A2: 
+# Description: 
+# Time: 
+# Space: 
+
+
+# A3: 
+# Description: 
+# Time: 
+# Space: 
